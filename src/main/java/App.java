adff4ae0bbab6e36d858edf1a858c53845e87d6b
alextinyakov <aleks_tii@mail.ru>
2022-12-06 23:26:26 +0300

    commit1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        System.out.println("Бины bean и bean2 ссылаются на один и тот же объект: " + (bean == bean2));
+
+        Cat cat1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Бины cat1 и cat2 ссылаются на один и тот же объект: " + (cat1 == cat2));
+
     }
 }
